071f3a0b8902509b6980c2c73040c488327ccd12
Arseniy Babahov <157617055+rqwhy@users.noreply.github.com>
2024-02-14 18:31:43 +0300

    Add files via upload

---
+++ proj3.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "тип данных\t| мин значение\t\t| максимальное значение\t| кол-во байт\t| описание" << endl;
+	cout << "short" << "\t\t";
+	cout << "| " << SHRT_MIN << "\t\t";
+	cout << "| " << SHRT_MAX << "\t\t\t";
+	cout << "| " << sizeof(short) << "\t\t";
+	cout << "| " << "целые числа" << endl;
+
+	cout << "int" << "\t\t";
+	cout << "| " << INT32_MIN << "\t\t";
+	cout << "| " << INT32_MAX << "\t\t";
+	cout << "| " << sizeof(int) << "\t\t";
+	cout << "| " << "целые числа" << endl;
+
+	cout << "long long" << "\t";
+	cout << "| " << LLONG_MIN << "\t";
+	cout << "| " << LLONG_MAX << "\t";
+	cout << "| " << sizeof(long) << "\t\t";
+	cout << "| " << "целые числа" << endl;
+
+	cout << "char" << "\t\t";
+	cout << "| " << CHAR_MIN << "\t\t\t";
+	cout << "| " << CHAR_MAX << "\t\t\t";
+	cout << "| " << sizeof(char) << "\t\t";
+	cout << "| " << "символ" << endl;
+
+	cout << "bool" << "\t\t";
+	cout << "| " << "0" << "\t\t\t";
+	cout << "| " << "1" << "\t\t\t";
+	cout << "| " << sizeof(bool) << "\t\t";
+	cout << "| " << "логика" << endl;
+
+	cout << "float" << "\t\t";
+	cout << "| " << FLT_MIN << "\t\t";
+	cout << "| " << FLT_MAX << "\t\t";
+	cout << "| " << sizeof(float) << "\t\t";
+	cout << "| " << "вещественный" << endl;
+
+	cout << "double" << "\t\t";
+	cout << "| " << DBL_MIN << "\t\t";
+	cout << "| " << DBL_MAX << "\t\t";
+	cout << "| " << sizeof(double) << "\t\t";
+	cout << "| " << "вещественный" << endl;
+}
