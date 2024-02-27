ea53633852406fdef3d60a69a054aaffb9883abd
Arseniy Babahov <157617055+rqwhy@users.noreply.github.com>
2024-02-27 18:14:09 +0300

    Add files via upload

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,110 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+	/*int a = 1,n;
+	cout << "Input n: ";
+	cin >> n;
+	while (n > 1) {
+		a *= n--;
+	}
+	cout << a;*/
+
+	//int max,min,a=0;
+	//cout << "Input min max: ";
+	//cin >> min >> max;
+	//if (min >= max) swap(min, max);
+	//while (a < max) {
+	//	cout << a++ << " ";
+	//}
+	//cout << endl <<  "thise numb between "<< min << max;
+	///*for (int a = min; a < max; a++) {
+	//	cout << a << " "; 
+	//}*/
+
+	/*int a;
+	cout << "Input a";
+	cin >> a;
+	for (int i = 0; i < a;i++) {
+		cout << "hello" << endl;
+	}*/
+
+	/*for (int min = 0; min <= 100; min+=2) {
+		cout << min << endl;
+	}*/
+
+	/*for (int a = 14; a <= 50; a+=9) {
+		cout << a << endl;
+	}*/
+
+	/*int a,count = 0;
+	cin >> a;
+
+	for (int i = a;i != 0; i /= 10) {
+		count *= 10;
+		count += i % 10;
+	}
+	cout << count;*/
+
+	/*char symbol;
+	int a;
+	cout << "Input symbol: ";
+	cin >> symbol;
+	cout << "Input a: ";
+	cin >> a;
+	for (int i = 0; i < a; i++) {
+		cout << symbol << " ";
+	}*/
+
+	/*char symbol1 = ' ';
+	char symbol2 = '*';
+	int row,column;
+	cout << "Input row: ";
+	cin >> row;
+	cout << "Input column: ";
+	cin >> column;
+	for (int i = 0; i < row; i++) {
+		cout << symbol2 << endl;
+	}
+	for (int i = 0; i < row-2; i++) {
+		for (int i = 0; i < column; i++) {
+			cout << symbol2 << symbol1 << symbol2 << endl;
+		}
+	}*/
+
+	/*int n;
+	cout << "Input n: ";
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int i1 = n-i; i1 < n; i1++) {
+			cout << "*";
+		}
+		cout << endl;
+	}*/
+
+	//int n;
+	//cout << "Input n: ";
+	//cin >> n;
+	//for (int i = 0; i < n; i++) {
+	//	for (int i1 = 0; i1 < n-i; i1++) {
+	//		cout << "*";
+	//	}
+	//	cout << endl;
+	//}
+
+
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int i1 = n-i; i1 < n; i1++) {
+			cout << " ";
+		}
+		for (int i1 = 0; i1 < n-i
+			; i1++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
